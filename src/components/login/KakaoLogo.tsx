e2a6dd69f93ba2b099bfd5d20964d297b6b8ec14
추성우 <cntjddn715@gmail.com>
2025-04-27 00:45:26 +0900

    chore :: 카카오 로고

---
+++ src/components/login/KakaoLogo.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function KakaoLogo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+    >
+      <path
+        opacity="0.902"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9 0.55542C4.29 0.55542 0 4.34142 0 7.54442C0 9.94442 1.558 12.0614 3.931 13.3194L2.933 16.9854C2.844 17.3104 3.213 17.5684 3.496 17.3814L7.873 14.4764C8.242 14.5124 8.618 14.5334 9 14.5334C13.97 14.5334 18 11.4044 18 7.54442C18 4.34142 13.97 0.55542 9 0.55542Z"
+        fill="black"
+      />
+    </svg>
+  );
+}
