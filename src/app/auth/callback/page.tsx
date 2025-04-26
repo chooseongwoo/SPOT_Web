981c3b378105006deb1568d5a58ff40f04de2232
추성우 <cntjddn715@gmail.com>
2025-04-27 02:30:48 +0900

    feat :: access_token & refresh_token 저장

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,4 +1,30 @@
+"use client";
+
+import { useHashParams } from "@/app/hooks";
+import { TOKEN } from "@/constants";
+import { Storage } from "@/storage";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function AuthCallbackPage() {
+  const router = useRouter();
+  const params = useHashParams();
+
+  useEffect(() => {
+    if (!params) return;
+
+    const accessToken = params.get(TOKEN.ACCESS);
+    const refreshToken = params.get(TOKEN.REFRESH);
+    if (accessToken && refreshToken) {
+      Storage.setItem(TOKEN.ACCESS, accessToken);
+      Storage.setItem(TOKEN.REFRESH, refreshToken);
+
+      router.replace("/");
+    } else {
+      alert("로그인에 실패했습니다.");
+    }
+  }, [router, params]);
+
   return (
     <main className="flex h-screen w-screen items-center justify-center">
       <span className="text-b1 text-black">로그인 중...</span>
