86300769137f40d5b045cd7eb1632a237e36a3e6
추성우 <cntjddn715@gmail.com>
2025-04-27 02:14:25 +0900

    feat :: 로그인 api 연결

---
+++ src/app/auth/callback/page.tsx
@@ -0,0 +1,7 @@
+export default function AuthCallbackPage() {
+  return (
+    <main className="flex h-screen w-screen items-center justify-center">
+      <span className="text-b1 text-black">로그인 중...</span>
+    </main>
+  );
+}
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,14 @@
+"use client"
 import KakaoLogo from "@/components/login/KakaoLogo";
+import { signInWithKakao } from "@/services/user/api";
 
 export default function LoginPage() {
   return (
     <main className="flex h-screen w-screen flex-col items-center">
-      <button className="absolute bottom-[21px] flex h-14 w-[327px] items-center rounded-xl bg-yellow px-3">
+      <button
+        className="absolute bottom-[21px] flex h-14 w-[327px] items-center rounded-xl bg-yellow px-3"
+        onClick={signInWithKakao}
+      >
         <KakaoLogo />
         <span className="w-full text-btn2 text-black">
           카카오로 3초만에 로그인
