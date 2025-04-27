215e7897afb6f8d632fc3af17aa691b4347043f2
추성우 <cntjddn715@gmail.com>
2025-04-28 00:04:16 +0900

    feat :: 로그인 페이지 퍼블리싱

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,31 @@
-"use client"
+"use client";
 import KakaoLogo from "@/components/login/KakaoLogo";
 import { signInWithKakao } from "@/services/user/api";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <main className="flex h-screen w-screen flex-col items-center">
+    <main className="flex h-screen w-screen flex-col items-center justify-center">
+      <Image
+        alt="로고 이미지"
+        src="/images/SplashImage.png"
+        width={90}
+        height={56}
+      />
+      <div className="mt-4 flex flex-col items-center gap-1">
+        <p className="text-t2 text-black">장소에 남기는 작은 이야기</p>
+        <p className="text-b1 text-gray-4">
+          스팟에 당신만의 기록을 남겨보세요!
+        </p>
+      </div>
+      <Image
+        alt="로그인 이미지"
+        src="/images/LoginImage.png"
+        width={200}
+        height={200}
+      />
       <button
-        className="absolute bottom-[21px] flex h-14 w-[327px] items-center rounded-xl bg-yellow px-3"
+        className="mt-6 flex h-14 w-[327px] items-center rounded-xl bg-yellow px-3"
         onClick={signInWithKakao}
       >
         <KakaoLogo />
