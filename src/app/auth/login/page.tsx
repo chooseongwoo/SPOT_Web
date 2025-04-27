0faf4d5830f7d33b7d6173bff3cd1cab46d138bc
추성우 <cntjddn715@gmail.com>
2025-04-28 00:15:36 +0900

    refactor :: 컴포넌트들 export 추가

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import KakaoLogo from "@/components/icons/KakaoLogo";
+import { KakaoLogo } from "@/components/icons";
 import { signInWithKakao } from "@/services/user/api";
 import Image from "next/image";
 
