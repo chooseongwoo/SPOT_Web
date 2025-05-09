6e9aab555ef3a723e9e4d5d77008de292f222f0e
추성우 <cntjddn715@gmail.com>
2025-05-09 12:22:04 +0900

    chore :: Chevron svg 변경

--- src/components/icons/ChevronIcon.tsx
+++ src/components/icons/ChevronIcon.tsx
@@ -4,6 +4,8 @@ import clsx from "clsx";
 export default function ChevronIcon({
   handleClick,
   direction = "left",
+  size = 24,
+  color = "#222",
 }: IconType) {
   const rotationClass = clsx({
     "rotate-0": direction === "left",
@@ -14,19 +16,19 @@ export default function ChevronIcon({
 
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="24"
-      viewBox="0 0 16 24"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
       fill="none"
-      onClick={handleClick}
+      xmlns="http://www.w3.org/2000/svg"
       className={rotationClass + " transition-transform"}
+      onClick={handleClick}
     >
       <path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M12.665 3.225C12.965 3.525 12.965 4.015 12.665 4.315L4.985 11.995L12.665 19.675C12.965 19.975 12.965 20.465 12.665 20.765C12.365 21.065 11.875 21.065 11.575 20.765L3.345 12.545C3.045 12.245 3.045 11.755 3.345 11.455L11.565 3.225C11.865 2.925 12.355 2.925 12.655 3.225H12.665Z"
-        fill="#212124"
+        d="M16.665 3.225C16.965 3.525 16.965 4.015 16.665 4.315L8.985 11.995L16.665 19.675C16.965 19.975 16.965 20.465 16.665 20.765C16.365 21.065 15.875 21.065 15.575 20.765L7.345 12.545C7.045 12.245 7.045 11.755 7.345 11.455L15.565 3.225C15.865 2.925 16.355 2.925 16.655 3.225H16.665Z"
+        fill={color}
       />
     </svg>
   );
