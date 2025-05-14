943279443ce9a053b8aee74ce8045eb6991816c5
추성우 <cntjddn715@gmail.com>
2025-05-14 22:34:38 +0900

    feat :: 메시지탭 헤드라인 구성

---
+++ src/app/(tabs)/message/Message.tsx
@@ -0,0 +1,9 @@
+export default function Message() {
+  return (
+    <div className="flex h-screen w-full flex-col">
+      <div className="flex w-full justify-start px-6 py-3">
+        <p className="text-headline text-black">메시지</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(tabs)/message/page.tsx
@@ -0,0 +1,5 @@
+import Message from "@/app/(tabs)/message/Message";
+
+export default function Page() {
+  return <Message />;
+}
