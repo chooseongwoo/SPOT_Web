1d6b016429a990bdb4bfcf53e7ecb4049a5f95ac
추성우 <cntjddn715@gmail.com>
2025-05-15 21:47:05 +0900

    feat :: 내 메시지 & 발견한 메시지 구분 탭

--- src/app/(tabs)/message/Message.tsx
+++ src/app/(tabs)/message/Message.tsx
@@ -1,8 +1,38 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Message() {
+  const [selectedTab, setSelectedTab] = useState<"mine" | "found">("mine");
+
   return (
     <div className="flex h-screen w-full flex-col">
       <div className="flex w-full justify-start px-6 py-3">
         <p className="text-headline text-black">메시지</p>
+      </div>
+      <div className="flex w-full justify-center gap-[18px] px-6">
+        <div
+          onClick={() => setSelectedTab("mine")}
+          className={`flex-1 cursor-pointer pb-[7px] text-center text-b2
+            ${
+              selectedTab === "mine"
+                ? "border-b border-b-black text-black"
+                : "border-b-0 text-gray-400"
+            }`}
+        >
+          내 메시지
+        </div>
+        <div
+          onClick={() => setSelectedTab("found")}
+          className={`flex-1 cursor-pointer pb-[7px] text-center text-b2
+            ${
+              selectedTab === "found"
+                ? "border-b border-b-black text-black"
+                : "border-b-0 text-gray-400"
+            }`}
+        >
+          발견한 메시지
+        </div>
       </div>
     </div>
   );
