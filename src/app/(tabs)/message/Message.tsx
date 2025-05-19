b4dd900b8c3c23e7ff82b3d5bb4caa984f5ae642
추성우 <cntjddn715@gmail.com>
2025-05-19 16:51:09 +0900

    feat :: 상단 탭 fixed 고정

--- src/app/(tabs)/message/Message.tsx
+++ src/app/(tabs)/message/Message.tsx
@@ -12,41 +12,43 @@ export default function Message() {
 
   return (
     <div className="flex h-screen w-full flex-col">
-      <div className="flex w-full justify-start px-6 py-3">
-        <p className="text-headline text-black">메시지</p>
-      </div>
-
-      <div className="relative flex w-full justify-center gap-[18px] px-6">
-        <div
-          onClick={() => setSelectedTab("mine")}
-          className={clsx(
-            "flex-1 cursor-pointer pb-[7px] text-center text-btn1",
-            selectedTab === "mine" ? "text-black" : "text-gray-400"
-          )}
-        >
-          내 메시지
-        </div>
-        <div
-          onClick={() => setSelectedTab("found")}
-          className={clsx(
-            "flex-1 cursor-pointer pb-[7px] text-center text-btn1",
-            selectedTab === "found" ? "text-black" : "text-gray-400"
-          )}
-        >
-          발견한 메시지
+      <div className="fixed flex w-full flex-col bg-white">
+        <div className="flex w-full justify-start px-6 py-3">
+          <p className="text-headline text-black">메시지</p>
         </div>
 
-        <motion.div
-          layout
-          transition={{ type: "spring", stiffness: 380, damping: 40 }}
-          className="absolute bottom-[-2px] h-[2px] w-[calc(50%-25px)] bg-black"
-          style={{
-            left: selectedTab === "mine" ? "calc(0% + 26px)" : "calc(50%)",
-          }}
-        />
+        <div className="relative flex w-full justify-center gap-[18px] px-6">
+          <div
+            onClick={() => setSelectedTab("mine")}
+            className={clsx(
+              "flex-1 cursor-pointer pb-[7px] text-center text-btn1",
+              selectedTab === "mine" ? "text-black" : "text-gray-400"
+            )}
+          >
+            내 메시지
+          </div>
+          <div
+            onClick={() => setSelectedTab("found")}
+            className={clsx(
+              "flex-1 cursor-pointer pb-[7px] text-center text-btn1",
+              selectedTab === "found" ? "text-black" : "text-gray-400"
+            )}
+          >
+            발견한 메시지
+          </div>
+
+          <motion.div
+            layout
+            transition={{ type: "spring", stiffness: 380, damping: 40 }}
+            className="absolute bottom-[-2px] h-[2px] w-[calc(50%-25px)] bg-black"
+            style={{
+              left: selectedTab === "mine" ? "calc(0% + 26px)" : "calc(50%)",
+            }}
+          />
+        </div>
       </div>
 
-      <div className="mt-[10px] flex flex-col gap-[10px] px-6 pb-24">
+      <div className="mt-[10px] flex flex-col gap-[10px] px-6 pb-24 pt-20">
         {selectedTab === "mine" &&
           messageData.map((message) => (
             <MineContainer key={message.id} {...message} />
