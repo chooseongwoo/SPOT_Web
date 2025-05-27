ddd4b535b04cd6e3618509edfc15e3ac0338db51
추성우 <cntjddn715@gmail.com>
2025-05-27 10:10:06 +0900

    feat :: 드래그 시 요소 사라짐

--- src/app/(history)/read/capsule/[id]/CapsuleDetail.tsx
+++ src/app/(history)/read/capsule/[id]/CapsuleDetail.tsx
@@ -26,7 +26,6 @@ export default function CapsuleDetail() {
 
     if (distance > 200) {
       setOpened(true);
-      console.log("타임캡슐 열림");
     }
 
     touchStartY.current = null;
@@ -39,20 +38,22 @@ export default function CapsuleDetail() {
         <CloseTab />
       </div>
 
-      <div className="absolute left-1/2 top-1/2 z-50 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-5">
-        <div className="flex flex-col items-center">
-          <ArrowWardIcon />
-          <ClickIcon />
+      {!opened && (
+        <div className="absolute left-1/2 top-1/2 z-50 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-5">
+          <div className="flex flex-col items-center">
+            <ArrowWardIcon />
+            <ClickIcon />
+          </div>
+          <p className="text-t2 text-white">위로 밀어 타임캡슐을 열어보세요</p>
         </div>
-        <p className="text-t2 text-white">위로 밀어 타임캡슐을 열어보세요</p>
-      </div>
+      )}
 
       <div
         className="absolute bottom-2 left-1/2 flex -translate-x-1/2 touch-none flex-col items-center"
         onTouchStart={handleTouchStart}
         onTouchEnd={handleTouchEnd}
       >
-        <Capsule3DIcon width={120} height={150} rotation={15} />
+        {!opened && <Capsule3DIcon width={120} height={150} rotation={15} />}
         <ShadowIcon />
       </div>
     </div>
