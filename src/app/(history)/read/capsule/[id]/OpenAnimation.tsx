c4c7b89dd4c8fa7ec939324fbc80c5ad095d77fc
추성우 <cntjddn715@gmail.com>
2025-05-30 23:24:02 +0900

    feat :: 캡슐 오픈 후 텍스트 추가

--- src/app/(history)/read/capsule/[id]/OpenAnimation.tsx
+++ src/app/(history)/read/capsule/[id]/OpenAnimation.tsx
@@ -1,5 +1,10 @@
 import { Capsule3DIcon } from "@/components/icons";
 
 export default function OpenAnimation() {
-  return <Capsule3DIcon width={120} height={150} />;
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-start gap-44 pt-36">
+      <p className="text-t2 text-white">타임캡슐이 열리고 있습니다...</p>
+      <Capsule3DIcon width={120} height={150} />
+    </div>
+  );
 }
