7648cc7e73303b2db9a2ffbe0a539fbe7721d062
추성우 <cntjddn715@gmail.com>
2025-06-02 09:21:37 +0900

    feat :: 캡슐 뚜껑 열리는 애니메이션 추가

--- src/app/(history)/read/capsule/[id]/OpenAnimation.tsx
+++ src/app/(history)/read/capsule/[id]/OpenAnimation.tsx
@@ -1,10 +1,19 @@
 import { Capsule3DIcon } from "@/components/icons";
+import { useEffect, useState } from "react";
 
 export default function OpenAnimation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsOpen(true);
+    }, 100);
+  }, []);
+
   return (
     <div className="flex h-screen w-full flex-col items-center justify-start gap-44 pt-36">
       <p className="text-t2 text-white">타임캡슐이 열리고 있습니다...</p>
-      <Capsule3DIcon width={120} height={150} />
+      <Capsule3DIcon width={120} height={150} isOpen={isOpen} />
     </div>
   );
 }
--- src/components/icons/Capsule3DIcon.tsx
+++ src/components/icons/Capsule3DIcon.tsx
@@ -1,21 +1,32 @@
 import clsx from "clsx";
 import { Top, Bottom } from "./Capsule3D";
 
+interface Capsule3DIconProps {
+  width?: number;
+  height?: number;
+  rotation?: number;
+  isOpen?: boolean;
+}
+
 export default function Capsule3DIcon({
   width = 138,
   height = 197,
   rotation = 0,
-}: {
-  width?: number;
-  height?: number;
-  rotation?: number;
-}) {
+  isOpen = false,
+}: Capsule3DIconProps) {
   return (
     <div
       className={clsx("relative")}
       style={{ width, height, transform: `rotate(${rotation}deg)` }}
     >
-      <Top />
+      <div
+        style={{
+          transition: "transform 0.5s",
+          transform: isOpen ? "translateY(-40px)" : "translateY(0)",
+        }}
+      >
+        <Top />
+      </div>
       <div className="absolute top-16">
         <Bottom />
       </div>
